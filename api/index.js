53c1d26d3f48b3303d80a89f8303dc3782b87ed0
Joenel <joenelsevellejo831@gmail.com>
2024-08-10 21:13:25 +0800

    new

--- api/index.js
+++ api/index.js
@@ -1,34 +1,28 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import UserRoutes from '../app/pages/userRoutes'; // Adjust the import path as necessary
 import ProtectedRoutes from '../app/pages/protectedRoutes'; // Adjust the import path as necessary
 import connectToMongoDB from './db'; // Adjust the import path as necessary
 
-
 dotenv.config(); // Load environment variables
 
-
 const server = express();
-const port = process.env.PORT || 9001;
 
+// Connect to MongoDB
 connectToMongoDB();
 
 // Middleware to parse JSON
 server.use(express.json());
 server.use(cors());
 
+// Define routes
+server.use('/api/users', UserRoutes);
+server.use('/api', ProtectedRoutes);
 
-      server.use('/api/users', UserRoutes);
-      server.use('/api', ProtectedRoutes);
-      
-      server.get("/", (req, res) => {
-        res.json("Hello");
-    })
+server.get("/", (req, res) => {
+    res.json("Hello");
+});
 
-      // Use Next.js request handler
-
-      server.listen(port, async () => {
-        console.log(`Server is listening on port ${port}`);
-      });
- 
+// Export the server as a handler for Vercel
+export default server;
