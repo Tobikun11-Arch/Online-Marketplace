df73b628723836e07d8b0cb8448dd0279daf39ce
Joenel <joenelsevellejo831@gmail.com>
2024-08-10 22:01:20 +0800

    new

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import UserRoutes from './userRoutes.js'; // Adjust the import path as necessary
-import ProtectedRoutes from './protectedRoutes.js'; // Adjust the import path as necessary
-import connectToMongoDB from './db.js'; // Adjust the import path as necessary
+import UserRoutes from './userRoutes'; // Adjust the import path as necessary
+import ProtectedRoutes from './protectedRoutes'; // Adjust the import path as necessary
+import connectToMongoDB from './db'; // Adjust the import path as necessary
 
 dotenv.config(); // Load environment variables
 const port = process.env.PORT || 9001;
