d9bf0adc9328daebde554bff9991a9227ebd11e2
Joenel <joenelsevellejo831@gmail.com>
2024-08-14 16:05:54 +0800

    new

---
+++ app/Option/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Option() {
+  return (
+    <>
+    
+    <div className="w-full h-screen bg-gray-900 gap-16 flex justify-center items-center">
+
+    <div className="w-64 h-96 bg-white">
+
+    </div>
+
+    <div className="w-64 h-96 bg-white">
+        
+    </div>
+
+    </div>
+
+    </>
+  )
+}
