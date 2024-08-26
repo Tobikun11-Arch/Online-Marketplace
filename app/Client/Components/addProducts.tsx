f825d2feed1ab9c460f351be9160bc98ba210f10
Joenel <joenelsevellejo831@gmail.com>
2024-08-26 18:02:04 +0800

    background

--- app/Client/Components/addProducts.tsx
+++ app/Client/Components/addProducts.tsx
@@ -32,7 +32,7 @@ export default function addProducts() {
 
      {/**Modal ======>*/}
      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box px-14 pt-14 pb-8 bg-white">
+        <div className="modal-box px-14 pt-14 pb-8 bg-white overflow-hidden">
           <form method="dialog">
             <button
               className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
@@ -45,12 +45,12 @@ export default function addProducts() {
           <form >
       <div>
         <label htmlFor="product-name" className='mt-10'>Product Name:</label>
-        <input type="text" id="product-name" placeholder='Shoes' className='border-b-2 marginLeft border-black outline-none' required />
+        <input type="text" id="product-name" placeholder='Shoes' className='border-b-2 bg-white marginLeft border-black outline-none' required />
       </div>
 
       <div className='flex flex-col'>
         <label htmlFor="product-description" className='mt-5'>Product Description:</label>
-        <textarea id="product-description" className='border-b-2 border-l-2 border-black outline-none mt-2 pl-2 resize-none' placeholder='Enter product description (200 characters max)' maxLength={200} rows={5} required />
+        <textarea id="product-description" className='border-b-2 border-l-2 bg-white border-black outline-none mt-2 pl-2 resize-none' placeholder='Enter product description (200 characters max)' maxLength={200} rows={5} required />
       </div>
 
       <div className='mt-5'>
