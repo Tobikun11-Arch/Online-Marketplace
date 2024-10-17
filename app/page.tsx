45bd9bc44f870fbdb810e8ce1a8873e9c407819a
Joenel <joenelsevellejo831@gmail.com>
2024-10-17 20:07:59 +0800

    done coding fordaday

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import HomePage from './Home Page/page'
-import LoginForm from './Client/auth/LoginForm'
 
 const Page = () => {
-  
 
   return (
     <div className="w-full h-screen">
