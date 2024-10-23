fe13868720d100b3beb5623012cf6e5bc9859a7e
Joenel <joenelsevellejo831@gmail.com>
2024-10-23 19:34:09 +0800

    Remove .server folder from repository

--- server/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { Register, Login } from "../controllers/userController";
-const router = Router();
-
-router.post('/register', Register);
-router.post('/login', Login);
-
-export default router;
