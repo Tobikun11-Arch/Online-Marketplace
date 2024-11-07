7599011d1c80386d16acab4a44f8c4bffe0a5cfb
Joenel <joenelsevellejo831@gmail.com>
2024-11-07 18:26:06 +0800

    added new api to deployment

--- app/buyer-dashboard/axios/dataStore.ts
+++ app/buyer-dashboard/axios/dataStore.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const userData = axios.create({
-    baseURL: 'http://localhost:5000/api/users/SellerProductlist' 
+    baseURL: process.env.NEXT_PUBLIC_MAINSHOP || 'http://localhost:5000/api/users/SellerProductlist'
 })
--- app/buyer-dashboard/components/Container/MainShopProduct.tsx
+++ app/buyer-dashboard/components/Container/MainShopProduct.tsx
@@ -8,42 +8,27 @@ interface productProps {
     isOpen: boolean
 }
 
-// const ProductCard = ({ products, isOpen } : productProps) => { Original version
-//     return (
-//         <div className={`${isOpen ? 'z-10' : ''}`}>
-//             <div className='w-full h-72 px-4 bg-transparent'>
-//                 {products.map((product)=> (
-//                     <>
-//                     <div key={product._id} className="relative w-full h-full flex flex-col mb-5 bg-white rounded-lg border">
-//                         <Image
-//                         layout='fill'
-//                         src={product.images[0]}
-//                         alt={`${product.productName}`}
-//                         className='px-10 pt-4 pb-10'
-//                         />
-//                         <ProductInfoCard productName={product.productName} productPrice={product.productPrice} productDiscount={product.productDiscount}/>
-//                     </div>  
-//                 </>
-//                 ))}
-//             </div>
-//         </div>
-//     )
-// }
-
-const ProductCard = ({ products, isOpen } : productProps) => {
+const MainShopProduct = ({ products, isOpen } : productProps) => {
     return (
         <div className={`${isOpen ? 'z-10' : ''}`}>
-            <div className='w-full h-72 px-4 bg-transparent'>
+            <div className='w-full h-96 px-4 bg-transparent'>
                 {products.map((product)=> (
-                    <>
-                    <div key={product._id} className="relative w-full h-full flex flex-col justify-center items-center mb-5 bg-white rounded-lg border">
-                       <h1>{product.productName}</h1>
-                    </div>  
-                </>
+                    <div key={product._id} className="relative w-full h-full flex flex-col justify-center items-center mb-5 bg-white hover:border-blue-500 rounded-lg border">
+                        <div className="relative w-3/4 h-3/4 aspect-w-1 aspect-h-1 hover:scale-105">
+                            <Image
+                                layout="fill"
+                                src={product.images[0]}
+                                alt={`${product.productName}`}
+                                className="object-contain object-center"
+                                placeholder = 'blur' // "empty" | "blur" | "data:image/..."
+                                blurDataURL='https://via.placeholder.com/100x100/CCCCCC/FFFFFF?text=Loading'
+                            />
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
     )
 }
 
-export default ProductCard
+export default MainShopProduct
