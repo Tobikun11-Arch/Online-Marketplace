6a597894316740e72ef35c71b57bdc0852a7133d
Joenel <joenelsevellejo831@gmail.com>
2024-12-03 21:04:30 +0800

    Trending add to cart problems - [✅]

--- app/buyer-dashboard/components/ui/AllproductCard.tsx
+++ app/buyer-dashboard/components/ui/AllproductCard.tsx
@@ -5,6 +5,10 @@ import { Products } from '../../entities/entities'
 import Link from 'next/link'
 import Image from 'next/image'
 
+interface productClick {
+    onClick:  React.MouseEventHandler<HTMLDivElement> | undefined
+}
+
 const fetchData = async() => {
     const response = await AllProducts.get('')
     const { TrendingProducts } = response.data
@@ -20,25 +24,25 @@ const AllproductCard = () => {
     return (
         <>
             {data?.map((datas, index)=> (
-                <Link href={`/buyer-dashboard/product/${datas._id}`} passHref key={index}>
-                    <div className='relative'>
-                        <div className="relative w-full hover:bg-gray-600 dark:hover:bg-gray-600 h-96 border rounded-lg cursor-pointer mt-3 bg-white dark:bg-transparent dark:border-[#333333]">
-                            <Image
-                                fill
-                                src={datas.images[0]}
-                                alt={`${datas.productName}`}
-                                className="object-contain object-center pb-24 px-10 aspect-w-1 aspect-h-1"
-                                placeholder="blur"
-                                blurDataURL="add new url later"
-                            />
-                        </div>
+                    <div className='relative' key={index}>
+                        <Link href={`/buyer-dashboard/product/${datas._id}`} passHref>
+                            <div className="relative w-full hover:bg-gray-600 dark:hover:bg-gray-600 h-96 border rounded-lg cursor-default mt-3 bg-white dark:bg-transparent dark:border-[#333333]">
+                                <Image
+                                    fill
+                                    src={datas.images[0]}
+                                    alt={`${datas.productName}`}
+                                    className="object-contain object-center pb-24 px-10 aspect-w-1 aspect-h-1"
+                                    placeholder="blur"
+                                    blurDataURL="add new url later"
+                                />
+                            </div>
+                        </Link>
                         <div className='absolute bottom-0 rounded-b-lg font-semibold text-white bg-gray-800 dark:bg-black w-full flex flex-col pr-3 pl-3 py-3 mt-4'>
                             <h1>{datas.productName}</h1>
                             <h1 className='text-gray-400'>₱{datas.productPrice}</h1>
                             <button className='bg-white mt-3 py-2 rounded-lg text-black'>Add to cart</button>
                         </div>
-                    </div>
-                </Link>
+                </div>
             ))}
         </>
     )
