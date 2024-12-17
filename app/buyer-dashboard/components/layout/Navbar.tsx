1bf0b729b3863748ce8ec54c3204a63ded55c317
Joenel <joenelsevellejo831@gmail.com>
2024-12-18 05:33:37 +0800

    added list if user is null in search

--- app/buyer-dashboard/components/layout/Navbar.tsx
+++ app/buyer-dashboard/components/layout/Navbar.tsx
@@ -6,15 +6,18 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { searchData } from '../../axios/dataStore'
 import { useUser } from '../../store/User'
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
+
 interface NavbarProps {
     className?: string
     isOpen: boolean
-} 
+}
 
 const Navbar = ({ className, isOpen }: NavbarProps) => {
     const [ search, setSearch ] = useState('')
     const { user } = useUser()
     const router = useRouter()
+    const [ open, setOpen ] = useState(false)
 
     const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
@@ -26,6 +29,8 @@ const Navbar = ({ className, isOpen }: NavbarProps) => {
             setSearch('');
         }
     };
+
+    const SearchHover = 'text-sm py-1 font-semibold px-2 rounded hover:bg-[#3333]' 
 
     return (
         <>
@@ -55,16 +60,52 @@ const Navbar = ({ className, isOpen }: NavbarProps) => {
                     </div>
                     <NavItems className="cursor-default flex gap-3 items-center text-sm text-gray-500"/>
                 </div>
-                <div className={`md:w-2/5 ${isOpen ? '' : 'relative'}`}>
-                    <Input type='search'
-                    value={search}
-                    KeyEnter={handleKeyDown}
-                    onChange={(e)=> setSearch(e.target.value)}
-                    className='bg-transparent border w-full rounded-md h-10 text-sm pl-2 pr-7 text-gray-800 dark:text-white outline-none' placeholder='Search for products...'/>
-                    <div className={`inset-y-0 right-0 flex items-center pr-3 ${isOpen ? 'hidden' : 'absolute'}`}>
-                    <Search strokeWidth={1.4} color='gray' size={15}/>
-                    </div>
-                </div>
+                <Popover className="relative md:w-2/5">
+                    <>
+                        <div className="relative w-full">
+                            <input
+                            type="search"
+                            value={search}
+                            onFocus={() => setOpen(true)}
+                            onBlur={() => setTimeout(() => setOpen(false), 10)}
+                            onKeyDown={handleKeyDown}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="bg-transparent border w-full rounded-md h-10 text-sm pl-2 pr-7 text-gray-800 dark:text-white outline-none"
+                            placeholder="Search for products..."
+                            />
+                            <div
+                            className={`absolute inset-y-0 right-3 flex items-center`}
+                            >
+                            <Search strokeWidth={1.4} color="gray" size={15} />
+                            </div>
+                        </div>
+                        {open && (
+                            <Popover.Panel
+                            className="absolute z-10 mt-1 w-full dark:bg-opacity-90 dark:backdrop-blur-sm backdrop-blur-md md:border dark:border-black dark:bg-black dark:text-white rounded-md shadow-lg"
+                            static
+                            >
+                                <div className="flex flex-col p-2">
+                                    {user?.Role === 'buyer' ? (
+                                        <h2 className={SearchHover}>user exist</h2>
+                                    ) : (
+                                        <>
+                                            <h2 className={SearchHover}>Air Jordan 4 Retro 'White Thunder'</h2>
+                                            <h2 className={SearchHover}>Adidas Samba</h2>
+                                            <h2 className={SearchHover}>New Balance 9060</h2>
+                                            <h2 className={SearchHover}>Nike Zoom Vomero 5</h2>
+                                            <h2 className={SearchHover}>Veja Campo</h2>
+                                            <h2 className={SearchHover}>Levi's 501 Original Jeans</h2>
+                                            <h2 className={SearchHover}>Hoka One One Bondi 8</h2>
+                                            <h2 className={SearchHover}>Carhartt WIP Detroit Jacket</h2>
+                                            <h2 className={SearchHover}>Patagonia Better Sweater Fleece Jacket</h2>
+                                            <h2 className={SearchHover}>Zara Satin Slip Dresses</h2>
+                                        </>
+                                    )}
+                                </div>
+                            </Popover.Panel>
+                        )}
+                    </>
+                </Popover>
             </div>
         </nav>
         </>
