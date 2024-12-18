9d2b601a9827de03a5a9af0a59a543b218389644
Joenel <joenelsevellejo831@gmail.com>
2024-12-18 08:11:00 +0800

    remove the error

--- app/buyer-dashboard/Popular/page.tsx
+++ app/buyer-dashboard/Popular/page.tsx
@@ -4,19 +4,20 @@ import { useProductData } from '../store/storeProduct'
 import ProductLists from '../components/pages/ProductList';
 
 const Page = () => {
-    const { product } = useProductData()
+    // const { product } = useProductData()
 
     //:Products in database must have sold and connected to buyer if there a buy method na
 // Set the product data in popular page to filter the value products sold in Products kung sino mas marami nasold naproducts
 
     return (
         <div className='min-h-screen bg-white dark:bg-[#171717]'>
-            <Header/>
+            {/* <Header/>
             <div className="px-5">
                 <h1 className='text-2xl font-semibold'>Popular Products Today</h1>
                 <p className='mb-3 text-xs text-gray-400'>Discover the most loved products this week.</p>
                 <ProductLists product={product}/>
-            </div>
+            </div> */}
+            <h1>Removed the error</h1>
         </div>
     )
 }
