381dbc242b41b2cf9a62e1caca60cae9c62f221e
Joenel <joenelsevellejo831@gmail.com>
2024-12-25 09:21:33 +0800

    added filtering showing all data in search history of user

--- app/buyer-dashboard/components/layout/Navbar.tsx
+++ app/buyer-dashboard/components/layout/Navbar.tsx
@@ -31,6 +31,7 @@ const Navbar = ({ className, isOpen }: NavbarProps) => {
     const router = useRouter()
     const [isPopoverOpen, setIsPopoverOpen] = useState(false);
     const { product, setHandler, handler, setProduct } = useProductData()
+    const [ show, set_show ] = useState(false)
     const SearchHover = 'text-sm py-1 font-semibold px-2 rounded hover:bg-[#3333]'
 
     //Fetch the products 
@@ -53,20 +54,23 @@ const Navbar = ({ className, isOpen }: NavbarProps) => {
 
     useEffect(()=> {
         if (user?.SearchData) {
-            if(user?.SearchData.length > 8) {
+            if(show) {
+                setHistory(user.SearchData)
+            }
+            else if(user?.SearchData.length > 8) {
                 const search_filter = user?.SearchData.length <= 10 ? user.SearchData : user.SearchData.slice(user.SearchData.length - 10)
                 setHistory(search_filter)
             } else {
                 setHistory(user.SearchData); //add the function later that will add to the local storage the new search data so it can show without fetching
             }
         }
+        console.log("Logs: ", user?.SearchData) //Test
     }, [user, handler])
 
     const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (typeof window !== 'undefined') {
             lineSpinner.register()
         }
-
         if (e.key === 'Enter') {
             if(search === "") {
                 console.log("No message")
@@ -88,13 +92,11 @@ const Navbar = ({ className, isOpen }: NavbarProps) => {
                     product,
                     search_similarity: fuzz.ratio(product.productName, search), 
                 }));
-
                 const filter_average = product_search.filter(item  => item.search_similarity > 70)
                 const product_data = filter_average.map(item => item.product)
-                //send the product data to page tsx of all  products
                 router.push('/buyer-dashboard/AllProducts')
                 setHandler(product_data)
-            }   
+            }
         }
     };
 
@@ -167,16 +169,18 @@ const Navbar = ({ className, isOpen }: NavbarProps) => {
                         {isPopoverOpen && (
                             <Popover.Panel
                             className="absolute z-10 mt-1 w-full dark:bg-opacity-90 bg-white dark:backdrop-blur-sm backdrop-blur-md md:border dark:border-black dark:bg-black dark:text-white rounded-md shadow-lg text-gray-500"
-                            static
-                            >
+                            static>
                                 <div className="flex flex-col p-2">
                                     {user?.Role === 'buyer' ? (
                                         <>
+                                        <h2 className='flex fixed self-end text-sm py-1 font-semibold px-2 underline' onClick={()=> set_show(true)}>Show search history</h2>
                                             {history.length > 0 ? (
                                             [...history].reverse().map((history, index) => (
-                                                <h2 key={index} className={SearchHover}>
-                                                    {history}
-                                                </h2>
+                                                <>
+                                                    <h2 key={index} className={SearchHover}>
+                                                        {history}
+                                                    </h2>
+                                                </>
                                             ))
                                             ) : (
                                                 <>
