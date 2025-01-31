cddff253111083334dd11de2c1a5c529f2e04bc0
Joenel <joenelsevellejo831@gmail.com>
2025-01-31 20:01:37 +0800

    fixed ssg error, changed to ssr

---
+++ app/buyer-dashboard/Popular/Popular.tsx
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react'
+import { ArrowLeft } from 'lucide-react';
+import ProductLists from '../components/pages/ProductList';
+import { Product } from '../entities/entities';
+
+interface Response {
+    popular_products: Product[]
+}
+
+const Page = ({ popular_products }: Response) => {
+    return (
+        <div className='min-h-screen bg-white dark:bg-[#171717] cursor-default'>
+            <div className="px-5 pt-5">
+                <div className='flex gap-1 items-center' onClick={() => window.history.back()}>
+                    <ArrowLeft size={20}/>
+                    <h2>Back</h2>
+                </div>
+                <h1 className='text-2xl font-semibold mt-2'>Popular Products Today</h1>
+                <p className='mb-3 text-xs text-gray-400'>Discover the most loved products this week.</p>
+                <ProductLists product={popular_products} />
+            </div>
+        </div>
+    )
+}
+
+export default Page
--- app/buyer-dashboard/Popular/page.tsx
+++ app/buyer-dashboard/Popular/page.tsx
@@ -1,33 +1,12 @@
-import React from 'react';
-import { ArrowLeft } from 'lucide-react';
-import ProductLists from '../components/pages/ProductList';
-import { Product } from '../entities/entities';
-import { AllProducts } from '../axios/dataStore';
+import { AllProducts } from "../axios/dataStore";
+import Popular from "./Popular";
 
-
-const Page = async () => {
-    let popular_products: Product[] = [];
-
+export default async function Page() {
     try {
-        const response = await AllProducts.get('');
-        popular_products = response.data.popular_products;
+        const response = await AllProducts.get('')
+        const { popular_products } = response.data
+        return <Popular popular_products={popular_products}/>
     } catch (error) {
-        console.error("Failed to fetch popular products:", error);
+        console.error(error)
     }
-
-    return (
-        <div className='min-h-screen bg-white dark:bg-[#171717] cursor-default'>
-            <div className="px-5 pt-5">
-                <div className='flex gap-1 items-center' onClick={() => window.history.back()}>
-                    <ArrowLeft size={20}/>
-                    <h2>Back</h2>
-                </div>
-                <h1 className='text-2xl font-semibold mt-2'>Popular Products Today</h1>
-                <p className='mb-3 text-xs text-gray-400'>Discover the most loved products this week.</p>
-                <ProductLists product={popular_products}/>
-            </div>
-        </div>
-    );
-};
-
-export default Page;
+}
