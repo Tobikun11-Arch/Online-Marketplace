8234bb815a9b913cf3dae013897b5f365c7f0977
Joenel <joenelsevellejo831@gmail.com>
2025-01-31 19:42:41 +0800

    fixed ssg error, changed to ssr

--- app/buyer-dashboard/Popular/page.tsx
+++ app/buyer-dashboard/Popular/page.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, } from 'react'
 import { ArrowLeft } from 'lucide-react';
 import ProductLists from '../components/pages/ProductList';
 import { Product } from '../entities/entities';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { AllProducts } from '../axios/dataStore';
 
 interface Response {
-    popular_products: Product[];
+    popular_products: Product[]
 }
 
-interface PageProps {
-    popular_products: Product[];
-}
+const Page = async () => {
+    const { data: Response } = useSuspenseQuery<Response>({
+        queryKey: ['popular_products'],
+        queryFn: async () => {
+            const response = await AllProducts.get('')
+            const { popular_products } = response.data
+            return { popular_products }
+        }
+    })
 
-const Page = ({ popular_products }: PageProps) => {
+    useEffect(()=> {
+        console.log("products: ", Response)
+    }, [Response])
+
     return (
         <div className='min-h-screen bg-white dark:bg-[#171717] cursor-default'>
             <div className="px-5 pt-5">
@@ -22,32 +32,10 @@ const Page = ({ popular_products }: PageProps) => {
                 </div>
                 <h1 className='text-2xl font-semibold mt-2'>Popular Products Today</h1>
                 <p className='mb-3 text-xs text-gray-400'>Discover the most loved products this week.</p>
-                <ProductLists product={popular_products}/>
+                <ProductLists product={Response.popular_products}/>
             </div>
         </div>
-    );
-};
-
-export async function getServerSideProps() {
-    try {
-        const response = await AllProducts.get('');
-        const { popular_products } = response.data;
-
-        return {
-            props: {
-                popular_products,
-            },
-        };
-    } catch (error) {
-        console.error("Failed to fetch popular products:", error);
-
-        // Return an empty array or handle the error as needed
-        return {
-            props: {
-                popular_products: [],
-            },
-        };
-    }
+    )
 }
 
-export default Page;
+export default Page
