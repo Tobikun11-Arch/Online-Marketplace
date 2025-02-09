3f2f1caedb729d14bad3d74bd719504c638c8e37
Joenel <joenelsevellejo831@gmail.com>
2025-02-09 19:23:01 +0800

    fixed the h

--- app/seller/components/dashboard-components/ComplexStyle.ts
+++ app/seller/components/dashboard-components/ComplexStyle.ts
@@ -1,2 +1,2 @@
-export const DataBody_01 = 'w-full shadow-lg bg-white p-4 rounded-lg'
-export const DataBody_02 = 'w-full  shadow-lg bg-white p-4 rounded-lg'
+export const DataBody_01 = 'w-full h-80 shadow-lg bg-white p-4 rounded-lg'
+export const DataBody_02 = 'w-full h-80 shadow-lg bg-white p-4 rounded-lg'
